f1643bf2d43a14ecc3902e68b62cf289426d5f2e
Chuong Tran <chuongninolxag@gmail.com>
2022-06-14 17:24:27 +0700

    add header default

--- src/components/layouts/DefaultLayout/Sidebar/index.js
+++ src/components/layouts/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
 
-export default function Sidebar() {
-  return <div>Sidebar</div>;
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+  return (
+    <aside className={cx('wrapper')}>
+      <h2>Sidebar</h2>
+    </aside>
+  );
 }
+
+export default Sidebar;
